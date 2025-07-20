e8ac54149b54f5b0a9dd01ad56e1add077a508e7
Viney Chhillar <ronaessi28chhillar@gmail.com>
2025-07-20 23:00:59 +0530

    Update Routes.py

--- App/Routes.py
+++ App/Routes.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify
-from app import app, bcrypt
+from app import app, bcrypt 
 from app.models import User, File
 from app.auth import authenticate_user 
 from flask_jwt_extended import jwt_required, get_jwt_identity
